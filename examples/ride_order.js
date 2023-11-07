3c0f25976e372340978e1c9e898821044bcbfda4
Daniel Kokotov <dkokotov@gmail.com>
2023-11-06 18:03:52 -0800

    Cover all the problem details types we support

--- examples/ride_order.js
+++ examples/ride_order.js
@@ -1,96 +1,99 @@
 /* eslint-disable max-len */
 import { randomUUID } from 'crypto';
-import { CNaughtApiClient, CNaughtError } from '@cnaught/cnaught-node-sdk';
+import {
+    CNaughtApiClient,
+    CNaughtError,
+    invalidParametersProblemType
+} from '@cnaught/cnaught-node-sdk';
 import inquirer from 'inquirer';
 import 'dotenv/config';
 
 (async () => {
     // Initialize your client with your CNaught API key
     const client = new CNaughtApiClient(process.env.CNAUGHT_API_KEY, {
-        hostname: 'api-local.cnaught.com',
-        port: 3011
+        hostname: 'api-stage.cnaught.com'
     });
 
-    const subaccounts = await client.getListOfSubaccounts();
-    let subaccountId = null;
-
-    if (subaccounts.data.length > 0) {
-        const isForSubaccount =
-            (
-                await inquirer.prompt({
-                    type: 'list',
-                    message:
-                        'This account contains subaccounts. Should the order be for the main account, or a subaccount?',
-                    name: 'mainOrSubaccount',
-                    choices: [
-                        {
-                            name: 'Main account',
-                            value: 'main'
-                        },
-                        {
-                            subaccount: 'A subaccount',
-                            value: 'subaccount'
-                        }
-                    ]
-                })
-            ).mainOrSubaccount === 'subaccount';
-
-        if (isForSubaccount) {
-            subaccountId = (
-                await inquirer.prompt({
-                    type: 'list',
-                    message: 'Subaccount to use',
-                    name: 'subaccount',
-                    choices: subaccounts.data.map((s) => ({
-                        name: s.name,
-                        value: s.id
-                    }))
-                })
-            ).subaccount;
-        }
-    }
-
-    const distance = (
-        await inquirer.prompt({
-            type: 'input',
-            name: 'distance',
-            message: 'How long was your ride (in km)?',
-            validate(value) {
-                const valid = !isNaN(parseFloat(value));
-                return valid || 'Please enter a number';
-            },
-            filter: Number
-        })
-    ).distance;
-
-    const onBehalf = (
-        await inquirer.prompt({
-            type: 'input',
-            name: 'onBehalf',
-            message: 'Who are you buying this for?'
-        })
-    ).onBehalf;
-    const description = onBehalf !== '' ? `On behalf of ${onBehalf}` : null;
-
-    let portfolioId = (
-        await inquirer.prompt({
-            type: 'input',
-            name: 'portfolioId',
-            message:
-                'Id of portfolio to use (leave blank for default portfolio)?'
-        })
-    ).portfolioId;
-    if (portfolioId === '') {
-        portfolioId = null;
-    }
-
-    console.log(
-        `You requested offsets for a ${distance} km ride, ${
-            subaccountId ? `using subaccount ${subaccountId}, ` : ''
-        } using portfolio ${portfolioId ?? 'default'} to fulfill`
-    );
-
     try {
+        const subaccounts = await client.getListOfSubaccounts();
+        let subaccountId = null;
+
+        if (subaccounts.data.length > 0) {
+            const isForSubaccount =
+                (
+                    await inquirer.prompt({
+                        type: 'list',
+                        message:
+                            'This account contains subaccounts. Should the order be for the main account, or a subaccount?',
+                        name: 'mainOrSubaccount',
+                        choices: [
+                            {
+                                name: 'Main account',
+                                value: 'main'
+                            },
+                            {
+                                subaccount: 'A subaccount',
+                                value: 'subaccount'
+                            }
+                        ]
+                    })
+                ).mainOrSubaccount === 'subaccount';
+
+            if (isForSubaccount) {
+                subaccountId = (
+                    await inquirer.prompt({
+                        type: 'list',
+                        message: 'Subaccount to use',
+                        name: 'subaccount',
+                        choices: subaccounts.data.map((s) => ({
+                            name: s.name,
+                            value: s.id
+                        }))
+                    })
+                ).subaccount;
+            }
+        }
+
+        const distance = (
+            await inquirer.prompt({
+                type: 'input',
+                name: 'distance',
+                message: 'How long was your ride (in km)?',
+                validate(value) {
+                    const valid = !isNaN(parseFloat(value));
+                    return valid || 'Please enter a number';
+                },
+                filter: Number
+            })
+        ).distance;
+
+        const onBehalf = (
+            await inquirer.prompt({
+                type: 'input',
+                name: 'onBehalf',
+                message: 'Who are you buying this for?'
+            })
+        ).onBehalf;
+        const description = onBehalf !== '' ? `On behalf of ${onBehalf}` : null;
+
+        let portfolioId = (
+            await inquirer.prompt({
+                type: 'input',
+                name: 'portfolioId',
+                message:
+                    'Id of portfolio to use (leave blank for default portfolio)?'
+            })
+        ).portfolioId;
+        if (portfolioId === '') {
+            portfolioId = null;
+        }
+
+        console.log(
+            `You requested offsets for a ${distance} km ride, ${
+                subaccountId ? `using subaccount ${subaccountId}, ` : ''
+            } using portfolio ${portfolioId ?? 'default'} to fulfill`
+        );
+
         const quote = await client.getRideQuote({
             distance_km: distance,
             portfolio_id: portfolioId
@@ -132,24 +135,18 @@ import 'dotenv/config';
     } catch (err) {
         if (err instanceof CNaughtError) {
             switch (err.problemDetails.type) {
-                case 'https://api.cnaught.com/v1/errors/invalid-parameters':
+                case invalidParametersProblemType:
                     console.log(
-                        'Parameters did not validate',
+                        err.problemDetails.title,
                         err.problemDetails.errors
                     );
                     break;
-                case 'https://api.cnaught.com/v1/errors/not-found':
-                    console.log(
-                        'Could not find something',
-                        err.problemDetails.title
-                    );
-                    break;
                 default:
-                    console.log('CNaught error', err.problemDetails.title);
+                    console.log(err.problemDetails.title);
                     break;
             }
         } else {
-            console.log('Generic error', err);
+            console.log('An error occurred', err);
         }
     }
 })();
--- src/models/CNaughtProblemDetails.ts
+++ src/models/CNaughtProblemDetails.ts
@@ -1,28 +1,124 @@
 import { ProblemDocument } from 'http-problem-details';
 
-type Override<T1, T2> = Omit<T1, keyof T2> & T2
+type Override<T1, T2> = Omit<T1, keyof T2> & T2;
 
-export const invalidParametersProblemType = 'https://api.cnaught.com/v1/errors/invalid-parameters' as const;
+export const invalidParametersProblemType =
+    'https://api.cnaught.com/v1/errors/invalid-parameters' as const;
 export type InvalidParametersProblemType = typeof invalidParametersProblemType;
 
-export const notFoundProblemType = 'https://api.cnaught.com/v1/errors/not-found' as const;
+export const invalidPaginationProblemType =
+    'https://api.cnaught.com/v1/errors/invalid-pagination' as const;
+export type InvalidPaginationProblemType = typeof invalidPaginationProblemType;
+
+export const invalidSubaccountProblemType =
+    'https://api.cnaught.com/v1/errors/invalid-subaccount' as const;
+export type InvalidSubaccountProblemType = typeof invalidSubaccountProblemType;
+
+export const notFoundProblemType =
+    'https://api.cnaught.com/v1/errors/not-found' as const;
 export type NotFoundProblemType = typeof notFoundProblemType;
+
+export const forbiddenProblemType =
+    'https://api.cnaught.com/v1/errors/forbidden' as const;
+export type ForbiddenProblemType = typeof forbiddenProblemType;
+
+export const idempotencyChangedPayloadProblemType =
+    'https://api.cnaught.com/v1/errors/idempotency-changed-payload' as const;
+export type IdempotencyChangedPayloadProblemType =
+    typeof idempotencyChangedPayloadProblemType;
+
+export const idempotencyConcurrentRequestsProblemType =
+    'https://api.cnaught.com/v1/errors/idempotency-concurrent-requests' as const;
+export type IdempotencyConcurrentRequestsProblemType =
+    typeof idempotencyConcurrentRequestsProblemType;
+
+export const idempotencyKeyTooLongProblemType =
+    'https://api.cnaught.com/v1/errors/idempotency-key-too-long' as const;
+export type IdempotencyKeyTooLongProblemType =
+    typeof idempotencyKeyTooLongProblemType;
 
 export type ValidationProblemDetails = Override<
     ProblemDocument,
     {
-        type: InvalidParametersProblemType
-        errors: Record<string, string[]>
+        status: 400;
+        type: InvalidParametersProblemType;
+        errors: Record<string, string[]>;
     }
->
+>;
+
+export type InvalidPaginationProblemDetails = Override<
+    ProblemDocument,
+    {
+        status: 400;
+        type: InvalidSubaccountProblemType;
+        detail: string;
+    }
+>;
+
+export type InvalidSubaccountProblemDetails = Override<
+    ProblemDocument,
+    {
+        status: 400;
+        type: InvalidSubaccountProblemType;
+    }
+>;
 
 export type NotFoundProblemDetails = Override<
     ProblemDocument,
     {
-        status: 404
-        type: NotFoundProblemType
+        status: 404;
+        type: NotFoundProblemType;
     }
->
+>;
 
-export type CNaughtProblemDetailsType = NotFoundProblemType | InvalidParametersProblemType;
-export type CNaughtProblemDetails = ValidationProblemDetails | NotFoundProblemDetails
+export type ForbiddenProblemDetails = Override<
+    ProblemDocument,
+    {
+        status: 403;
+        type: ForbiddenProblemType;
+    }
+>;
+
+export type IdempotencyChangedPayloadProblemDetails = Override<
+    ProblemDocument,
+    {
+        status: 422;
+        type: IdempotencyChangedPayloadProblemType;
+    }
+>;
+
+export type IdempotencyConcurrentRequestsProblemDetails = Override<
+    ProblemDocument,
+    {
+        status: 409;
+        type: IdempotencyConcurrentRequestsProblemType;
+    }
+>;
+
+export type IdempotencyKeyTooLongProblemDetails = Override<
+    ProblemDocument,
+    {
+        status: 400;
+        type: IdempotencyKeyTooLongProblemType;
+    }
+>;
+
+export type CNaughtProblemType =
+    | NotFoundProblemType
+    | ForbiddenProblemType
+    | InvalidParametersProblemType
+    | InvalidPaginationProblemType
+    | InvalidSubaccountProblemType
+    | IdempotencyKeyTooLongProblemType
+    | IdempotencyChangedPayloadProblemType
+    | IdempotencyConcurrentRequestsProblemType;
+
+export type CNaughtProblemDetails =
+    | NotFoundProblemDetails
+    | ForbiddenProblemDetails
+    | ValidationProblemDetails
+    | InvalidPaginationProblemDetails
+    | InvalidSubaccountProblemDetails
+    | IdempotencyKeyTooLongProblemDetails
+    | IdempotencyChangedPayloadProblemDetails
+    | IdempotencyConcurrentRequestsProblemDetails;
