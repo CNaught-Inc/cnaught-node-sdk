cdef7c29965f58a687c64d5ba535dac62ead4a7b
Jason Chen <jasonschen5@gmail.com>
2024-02-27 20:17:47 -0500

    Fix checkout session notification config option

--- src/models/CheckoutSessionOptions.ts
+++ src/models/CheckoutSessionOptions.ts
@@ -7,5 +7,5 @@ export interface CheckoutSessionOptions {
     cancel_url: string;
     portfolio_id?: string;
     description?: string;
-    notificationConfig?: NotificationConfig;
+    notification_config?: NotificationConfig;
 }
