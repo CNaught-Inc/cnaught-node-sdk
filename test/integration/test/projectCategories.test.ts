12ed1dc716c17aafc35772bf46ba6ee00bb44182
Dan Kokotov <dkokotov@gmail.com>
2024-12-18 12:31:06 -0800

    fix tests

--- test/integration/test/projectCategories.test.ts
+++ test/integration/test/projectCategories.test.ts
@@ -10,7 +10,7 @@ test('get project category by id', async () => {
         'This category is for sandbox use only and does not represent any real data.'
     );
     expect(projectCategory.primary_image_url).toBe(
-        'https://assets.cnaught.com/64fd900fcf6f93409fc7ff21/6524ae6d35a66c6068f6bbe1_64cabe99f9ba36af8ecb171c_impact-portfolio.jpeg'
+        'https://assets.cnaught.com/64fd900fcf6f93409fc7ff21/67466d461956d2f8b3ccb4bb_64cabe99f9ba36af8ecb171c_impact-portfolio.webp'
     );
     expect(projectCategory.projects).toHaveLength(1);
     expect(projectCategory.projects[0].name).toBe('Sandbox Project A');
